f8e2dee4330c20695b1a1e3ece69af621db6441e
Davon <128499777+DavonQ@users.noreply.github.com>
2023-07-08 13:39:53 +0100

    Update server.py

--- final_project/server.py
+++ final_project/server.py
@@ -1,24 +1,29 @@
 from machinetranslation import translator
-from flask import Flask, render_template, request
-import json
+from flask import Flask, render_template, request, jsonify
 
-app = Flask("Web Translator")
+app = Flask("Web Translator", template_folder="machinetranslation/templates")
 
 @app.route("/englishToFrench")
 def englishToFrench():
     textToTranslate = request.args.get('textToTranslate')
-    # Write your code here
-    return "Translated text to French"
+    if not textToTranslate:
+        return jsonify({'error': 'Invalid input: Text to translate is missing.'}), 400
+
+    translatedText = translator.english_to_french(textToTranslate)
+    return jsonify({'translation': translatedText})
 
 @app.route("/frenchToEnglish")
 def frenchToEnglish():
     textToTranslate = request.args.get('textToTranslate')
-    # Write your code here
-    return "Translated text to English"
+    if not textToTranslate:
+        return jsonify({'error': 'Invalid input: Text to translate is missing.'}), 400
+
+    translatedText = translator.french_to_english(textToTranslate)
+    return jsonify({'translation': translatedText})
 
 @app.route("/")
 def renderIndexPage():
-    # Write the code to render template
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
